a8dfe146e8c25b3f6e97e6f05aed98ca489344b1
julian-git <julian-git@gmx.de>
2013-01-25 20:20:36 +0100

    lps now solve correctly in any given week

--- sql/old_prices.py
+++ sql/old_prices.py
@@ -1,4 +1,6 @@
 from MySQLdb import connect
+from subprocess import call 
+from string import replace
 
 def get_db():
     return connect(user="aixada", passwd="aixada", db="aixada")
@@ -51,33 +53,81 @@ group by product_id""",
               (date, uf_id,))
     return c.fetchall()
 
-def make_eqs(uf, products, spent, eqs):
+def xname(date, product):
+    return 'x' + str(product) + '_' + date
+
+def ptolname(date, uf):
+    return 'p' + str(uf) + '_' + date
+
+def ntolname(date, uf):
+    return 'n' + str(uf) + '_' + date
+
+def make_eqs(date, uf, products, spent, eqs):
     if spent is not None:
-        eqstr = 'uf' + str(uf) + ': ' + ' + '.join([str(p[0]) + ' x' + str(p[1]) for p in products]) + ' + ptol - ntol = ' + str(spent)
+        eqstr = 'uf' + str(uf) + '_' + date + ': ' + \
+            ' + '.join([str(p[0]) + ' ' + xname(date, p[1]) for p in products]) + \
+            ' + ' + ptolname(date, uf) + \
+            ' - ' + ntolname(date, uf) + \
+            ' = ' + str(spent)
         eqs.append(eqstr)
 
-def max_lp(eqs, vars, special_var):
-    return 'minimize 1000 ptol + 1000 ntol + x' + str(special_var) + '\nsubject to\n' + \
+def lp(date, eqs, ufs, product_vars, special_product_var, maximize=1):
+    if maximize==1:
+        sign = ' - '
+    else:
+        sign = ' + '
+    return 'minimize ' + \
+        ' + '.join(['1000 ' + ptolname(date, uf) + ' + 1000 ' + ntolname(date, uf) for uf in ufs]) + \
+        sign + str(special_product_var) + '\nsubject to\n' + \
         '\n'.join(eqs) + \
-        '\nbounds\nx' + \
-        ' >= 0\nx'.join([str(v) for v in varset]) + \
-        '\n'.join([' >= 0', 'ptol >= 0', 'ntol >= 0'])
+        '\nbounds\n' + \
+        ' >= 0\n'.join(product_vars) + \
+        ' >= 0\n' + \
+        '\n'.join([ptolname(date, uf) + ' >= 0' for uf in ufs]) + \
+        '\n' + \
+        '\n'.join([ntolname(date, uf) + ' >= 0' for uf in ufs])
+
+def bound(date, eqs, ufs, product_vars, v, maximize):
+    fout = open('prob.lp', 'w')
+    fout.write(lp(date, eqs, ufs, product_vars, v, maximize))
+    fout.close()
+    call(['gurobi_cl', 'OutputFlag=0', 'ResultFile=prob.sol', 'prob.lp'])
+    return solution(v)
+
+def solution(special_product_var):
+    fin = open('prob.sol', 'r')
+    first = True
+    for line in fin:
+        if first:
+            first = False
+            try:
+                obj = line.split('=')[1]
+            except IndexError:
+                return None
+            continue
+        linearray = line.split(' ')
+        if linearray[0] == special_product_var:
+            return float(linearray[1])
+
 
 if __name__ == "__main__":
     db = get_db()
     eqs = []
     vars = []
-    date = '2005-01-26'
-    for _uf in ufs_at_date(db, date):
-        uf = _uf[0]
+    date = '2005-01-12'
+    vdate = replace(date, '-', '_')
+    ufs = [_uf[0] for _uf in ufs_at_date(db, date)]
+    for uf in ufs:
         pb = products_bought(db, uf, date)
-        print uf, pb
         for p in pb:
-            vars.append(p[1])
+            vars.append(xname(vdate, p[1]))
         ts = total_spent(db, uf, date)
-        make_eqs(uf, pb, ts, eqs)
-    varset = set(vars)
-    for v in varset:
-        f = open('prob.lp', 'w')
-        f.write(max_lp(eqs, vars, v))
-        break
+        make_eqs(vdate, uf, pb, ts, eqs)
+    product_vars = set(vars)
+    bounds = dict()
+    for v in product_vars:
+        min_res = bound(vdate, eqs, ufs, product_vars, v, 0)
+        max_res = bound(vdate, eqs, ufs, product_vars, v, 1)
+        bounds[v] = [min_res, max_res]
+        
+    print bounds
