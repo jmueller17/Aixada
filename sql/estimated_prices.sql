99b729d50ea111c017819460b4df9accccefc6bb
julian-git <julian-git@gmx.de>
2013-01-26 23:15:22 +0100

    removed linear program generators again; it was fun while it lasted, but we found the real product prices in an old dump

--- sql/estimated_prices.sql
+++
@@ -1,9 +0,0 @@
-drop table if exists aixada_estimated_prices;
-create table aixada_estimated_prices (
-  product_id     	int		not null,
-  ts                  	timestamp       not null default current_timestamp,
-  min_estimated_price   decimal(10,2)   default null,
-  max_estimated_price   decimal(10,2)   default null,
-  true_price 		decimal(10,2)   default null,
-  primary key (product_id, ts)  
-) ENGINE=InnoDB DEFAULT CHARACTER SET utf8;
