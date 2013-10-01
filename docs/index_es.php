0e5dcca3fd60ef40de59eeacd1c72ea4c6426cb6
Luis Alonzo <lupa18@gambino.dreamhost.com>
2013-09-30 21:33:09 -0700

    Luis Alonzo: Agrego algo de ayuda al usuario

--- docs/index_es.php
+++ docs/index_es.php
@@ -6,39 +6,43 @@
 <style>
 	div#stageWrap {width:80%; margin:10px auto;}
 	body {font-family:Arial;}
-	.faq-item {margin-bottom:50px;}
+	.faq-item {margin-bottom:20px;}
 </style>
 </head>
 <body>
 <div id="stageWrap">
-<h1>FAQ</h1>
-<a name="top"></a>
-<ol>
-	<li><a href="#printOrder">Cómo imprimir un pedido</a></li>
-	<li><a href="#anyOtherTheme">any other topic...</a></li>
-</ol>
+	<h1>Preguntas Frecuentes(FAQ)</h1>
+	<a name="top"></a>
+	<ul>
+		<li><a href="#makeOrder">¿Cómo hago mi pedido?</a></li>
+		<li><a href="#printOrder">¿Cómo imprimo mi pedido?</a></li>
+	</ul>
 
-
-<a name="printOrder"></a>
 <div class="faq-item">
-<h2>How to print an order</h2>
-<p>Explanation starts there ...</p>
-
-
-<a href="#top">Back to top</a>
+	<h2>¿Cómo hago mi pedido?</h2>
+	<a name="makeOrder"></a>
+	<ol>
+		<li>Hacer clic en "Siguente pedido"</li>
+		<li>Seleccionar "Por proveedor" o "Por categoría de productos". Aparecerán los productos habilitados para la fecha indicada</li>
+		<li>Completar las cantidades de cada producto y al hacer clic fuera o tabulador, la cantidad queda ingresada</li>
+		<li>Finalmente en el panel de resumen (a la derecha), hacer clic en "Guardar"</li>
+	<ol>
 </div>
+<a href="#top">Back to top</a>
 
-
-<a name="anyOtherTheme"></a>
 <div class="faq-item">
-<h2>Any other title</h2>
-<p>Explanation starts there ...</p>
-
-
+	<h2>¿Cómo imprimo mi pedido?</h2>
+	<a name="printOrder"></a>
+	<p>Para obtener una versión imprimible del pedido, basta hacer lo siguiente: </p>
+        <ol>
+                <li>Hacer clic en "Mi(s) pedido(s)"</li>
+                <li>En la parte superior derecha de los pedidos, donde termina la línea del título, aparecerá un ícono de impresora. Al hacer clic en él, obtendrá una versión imprimible de su(s) pedido(s)</li>
+        <ol>
+</div>
 <a href="#top">Back to top</a>
-</div>
 
 
 </div>
+
 </body>
 </html>
