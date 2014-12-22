108dab4ae4f2192c5ffb3c87f812146b9ccccf4c
Jörg Müller <joerg@toytic.com>
2014-12-22 17:24:02 +0100

    sepa lib

---
+++ php/external/sepa/README.md
@@ -0,0 +1,109 @@
+php-sepa-xml
+============
+
+Master: [![Build Status](https://secure.travis-ci.org/digitick/php-sepa-xml.png?branch=master)](http://travis-ci.org/digitick/php-sepa-xml)
+
+SEPA file generator for PHP.
+
+Creates an XML file for a Single Euro Payments Area (SEPA) Credit Transfer and Direct Debit.
+
+License: GNU Lesser General Public License v3.0
+
+
+The versions of the standard followed are:
+* _pain.001.002.03_ (or _pain.001.001.03_) for credits
+* and _pain.008.002.02_ (or _pain.008.001.02_) for debits
+
+Institutions and associations that should accept this format:
+* Deutsche Kreditwirtschaft
+* Fédération bancaire française
+
+However, always verify generated files with your bank before using!
+
+
+##Installation
+###Composer
+This library is available in packagist.org, you can add it to your project
+via Composer.
+
+In the "require" section of your composer.json file:
+
+Always up to date (bleeding edge, API *not* guaranteed stable)
+```javascript
+"digitick/sepa-xml" : "dev-master"
+```
+
+No namespaces, only bugfixes
+```javascript
+"digitick/sepa-xml" : "dev-no_namespace"
+```
+
+Specific minor version, API stability
+```javascript
+"digitick/sepa-xml" : "0.10.*"
+```
+
+##Sample Usage DirectDebit with Factory
+```php
+//Set the initial information
+$directDebit = TransferFileFacadeFactory::createDirectDebit('test123', 'Me');
+
+// create a payment, it's possible to create multiple payments,
+// "firstPayment" is the identifier for the transactions
+$directDebit->addPaymentInfo('firstPayment', array(
+	'id' 					=> 'firstPayment',
+	'creditorName' 			=> 'My Company',
+	'creditorAccountIBAN'	=> 'FI1350001540000056',
+	'creditorAgentBIC' 		=> 'PSSTFRPPMON',
+	'seqType'				=> PaymentInformation::S_ONEOFF,
+	'creditorId'			=> 'DE21WVM1234567890'
+));
+// Add a Single Transaction to the named payment
+$directDebit->addTransfer('firstPayment', array(
+	'amount'				=> '500',
+	'debtorIban'			=> 'FI1350001540000056',
+	'debtorBic'				=> 'OKOYFIHH',
+	'debtorName'			=> 'Their Company',
+	'debtorMandate'			=>  'AB12345',
+	'debtorMandateSignDate'	=> '13.10.2012',
+	'remittanceInformation'	=> 'Purpose of this direct debit'
+));
+// Retrieve the resulting XML
+$directDebit->asXML();
+```
+
+##Extended Usage Credit Transfer
+```php
+// Create the initiating information
+$groupHeader = new GroupHeader('SEPA File Identifier', 'Your Company Name');
+$sepaFile = new CustomerCreditTransferFile($groupHeader);
+
+$transfer = new CustomerCreditTransferInformation(
+    '0.02', // Amount
+    'FI1350001540000056', //IBAN of creditor
+    'Their Corp' //Name of Creditor
+);
+$transfer->setBic('OKOYFIHH'); // Set the BIC explicitly
+$transfer->setRemittanceInformation('Transaction Description');
+
+// Create a PaymentInformation the Transfer belongs to
+$payment = new PaymentInformation(
+    'Payment Info ID',
+    'FR1420041010050500013M02606', // IBAN the money is transferred from
+    'PSSTFRPPMON',  // BIC
+    'My Corp' // Debitor Name
+);
+// It's possible to add multiple Transfers in one Payment
+$payment->addTransfer($transfer);
+
+// It's possible to add multiple payments to one SEPA File
+$sepaFile->addPaymentInformation($payment);
+
+// Attach a dombuilder to the sepaFile to create the XML output
+$domBuilder = DomBuilderFactory::createDomBuilder($sepaFile);
+
+// Or if you want to use the format 'pain.001.001.03' instead
+// $domBuilder = DomBuilderFactory::createDomBuilder($sepaFile, 'pain.001.001.03');
+
+$domBuilder->asXml();
+```
---
+++ php/external/sepa/lib/Digitick/Sepa/GroupHeader.php
@@ -0,0 +1,214 @@
+<?php
+/**
+ * SEPA file generator.
+ *
+ * @copyright © Digitick <www.digitick.net> 2012-2013
+ * @copyright © Blage <www.blage.net> 2013
+ * @license GNU Lesser General Public License v3.0
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Lesser Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace Digitick\Sepa;
+
+use Digitick\Sepa\DomBuilder\DomBuilderInterface;
+use Digitick\Sepa\Util\StringHelper;
+
+class GroupHeader
+{
+    /**
+     * Whether this is a test Transaction
+     *
+     * @var boolean
+     */
+    protected $isTest;
+
+    /**
+     * @var string Unambiguously identify the message.
+     */
+    protected $messageIdentification;
+
+    /**
+     * The initiating Party for this payment
+     *
+     * @var string
+     */
+    protected $initiatingPartyId;
+
+    /**
+     * @var int
+     */
+    protected $numberOfTransactions = 0;
+
+    /**
+     * @var int
+     */
+    protected $controlSumCents = 0;
+
+    /**
+     * @var string
+     */
+    protected $initiatingPartyName;
+
+    /**
+     * @var \DateTime
+     */
+    protected $creationDateTime;
+
+    /**
+     * Should the bank book multiple transaction as a batch
+     *
+     * @var int
+     */
+    protected $batchBooking = null;
+
+    /**
+     * @param $messageIdentification
+     * @param $isTest
+     * @param $initiatingPartyName
+     */
+    function __construct($messageIdentification, $initiatingPartyName, $isTest = false)
+    {
+        $this->messageIdentification = $messageIdentification;
+        $this->isTest = $isTest;
+        $this->initiatingPartyName = StringHelper::sanitizeString($initiatingPartyName);
+        $this->creationDateTime = new \DateTime();
+    }
+
+    public function accept(DomBuilderInterface $domBuilder)
+    {
+        $domBuilder->visitGroupHeader($this);
+    }
+
+    /**
+     * @param int $controlSumCents
+     */
+    public function setControlSumCents($controlSumCents)
+    {
+        $this->controlSumCents = $controlSumCents;
+    }
+
+    /**
+     * @return int
+     */
+    public function getControlSumCents()
+    {
+        return $this->controlSumCents;
+    }
+
+    /**
+     * @param string $initiatingPartyId
+     */
+    public function setInitiatingPartyId($initiatingPartyId)
+    {
+        $this->initiatingPartyId = $initiatingPartyId;
+    }
+
+    /**
+     * @return string
+     */
+    public function getInitiatingPartyId()
+    {
+        return $this->initiatingPartyId;
+    }
+
+    /**
+     * @param string $initiatingPartyName
+     */
+    public function setInitiatingPartyName($initiatingPartyName)
+    {
+        $this->initiatingPartyName = StringHelper::sanitizeString($initiatingPartyName);
+    }
+
+    /**
+     * @return string
+     */
+    public function getInitiatingPartyName()
+    {
+        return $this->initiatingPartyName;
+    }
+
+    /**
+     * @param boolean $isTest
+     */
+    public function setIsTest($isTest)
+    {
+        $this->isTest = $isTest;
+    }
+
+    /**
+     * @return boolean
+     */
+    public function getIsTest()
+    {
+        return $this->isTest;
+    }
+    
+    /**
+     * @param boolean $batchBooking
+     */
+    public function setBatchBooking($batchBooking)
+    {
+        $this->batchBooking = $batchBooking;
+    }
+
+    /**
+     * @return int|null
+     */
+    public function getBatchBooking()
+    {
+        return $this->batchBooking;
+    }
+
+    /**
+     * @param string $messageIdentification
+     */
+    public function setMessageIdentification($messageIdentification)
+    {
+        $this->messageIdentification = $messageIdentification;
+    }
+
+    /**
+     * @return string
+     */
+    public function getMessageIdentification()
+    {
+        return $this->messageIdentification;
+    }
+
+    /**
+     * @param int $numberOfTransactions
+     */
+    public function setNumberOfTransactions($numberOfTransactions)
+    {
+        $this->numberOfTransactions = $numberOfTransactions;
+    }
+
+    /**
+     * @return int
+     */
+    public function getNumberOfTransactions()
+    {
+        return $this->numberOfTransactions;
+    }
+
+    /**
+     * @return \DateTime
+     */
+    public function getCreationDateTime()
+    {
+        return $this->creationDateTime;
+    }
+
+}
