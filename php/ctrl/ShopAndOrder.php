bf808a661fa822c15346f848e73d428723a31da7
Xavier Mamano <xavier.mamano@gmail.com>
2016-12-06 16:28:19 +0100

    Remove unused some sql procedures

--- php/ctrl/ShopAndOrder.php
+++ php/ctrl/ShopAndOrder.php
@@ -87,27 +87,6 @@ try{
   			printXML(stored_query_XML_fields('get_shop_cart', get_param('date'), get_session_uf_id(),0,0)); 
 			exit; 
 
- 		    	
-	    	
-		/*
-	    case 'makeFavoriteOrderCart':
-	        printXML(stored_query_XML_fields('make_favorite_order_cart', $uf_logged_in, $the_date, $_REQUEST['cart_name']));
-	        exit;
-	
-	    case 'getFavoriteOrderCarts':
-	        printXML(stored_query_XML_fields('get_favorite_order_carts', $uf_logged_in));
-	        exit;
-	
-	    case 'getFavoriteOrdersOfCart':
-	        printXML(stored_query_XML_fields('products_for_favorite_order', $uf_logged_in, $_REQUEST['cart_id']));
-	        exit;
-	
-	    case 'deleteFavoriteOrderCart':
-	        printXML(stored_query_XML_fields('delete_favorite_order_cart', $uf_logged_in, $_REQUEST['cart_id']));
-	        exit;
-	
-		*/
-
     	default:  
     	 //throw new Exception("ctrlShopAndOrder: oper={$_REQUEST['oper']} not supported");  
         break;
