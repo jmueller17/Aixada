5723493f4c21bd80e3c67d8ad6e3198124c06655
Xavier Mamano <xavier.mamano@gmail.com>
2016-12-17 21:05:26 +0100

    Allow the use of ssl and dump messages in development.
    Emails validation are changed to throw exceptions with explanatory text of the reason.
    (now Swift_Validate::email is not used)

--- php/utilities/general.php
+++ php/utilities/general.php
@@ -355,11 +355,11 @@ function to_js_str($text) {
  * @param array $options valid keys are: 'reply_to', 'cc', 'bcc'
  * @return boolean as response of php mail.
  */
-function send_mail($to, $subject, $bodyHTML, $options=null) {
+function send_mail($to, $subject, $bodyHTML, $options=null)
+{
     if (!isset($options)) {
         $options = array();
     }
-    $cfg = configuration_vars::get_instance();
 
     // get URL of aixada root
     $pos_root = strrpos($_SERVER['SCRIPT_NAME'], '/php/ctrl/');
@@ -370,76 +370,111 @@ function send_mail($to, $subject, $bodyHTML, $options=null) {
     $url_root = (isset($_SERVER['HTTP_HOST']) ? 
                     $_SERVER['HTTP_HOST'] : $_SERVER['SERVER_NAME']).
                 substr($_SERVER['SCRIPT_NAME'],0,$pos_root);
-
     // get HTML message
-    $subject = $cfg->coop_name.': '.$subject;
+    $subject = get_config('coop_name') . ': ' . $subject;
     $messageHTML = 
         '<html><head><title>'.$subject."</title></head>\r\n".
         '<body style="font-family: Lucida Grande, Lucida Sans, Arial, sans-serif;">'.
         "\r\n".$bodyHTML."\r\n".
         '<hr><div style="color:#888; text-align: center;">'.
-                $cfg->coop_name.': <a href="'.
+                get_config('coop_name') . ': <a href="'.
                     ($ssl_on ? 'https://' : 'http://').
                     $url_root.
                     '/index.php" style="color:#888;">'.$url_root.'</a>'.
             "</div>\r\n".
         "</body></html>";
-    if (get_config('email_SMTP_host', '') === '') { // phpMail
-      $from = $cfg->admin_email;
-      $headers = 
-          'From: '.$from."\r\n".
-          'Reply-To: '.
-              (isset($options['reply_to']) ? $options['reply_to'] : $from)."\r\n".
-          (isset($options['cc']) ? 'Cc:'.$options['cc']."\r\n" : '').
-          (isset($options['bcc']) ? 'Bcc:'.$options['bcc']."\r\n" : '').
-          'Return-Path: '.$from."\r\n".
-          "X-Mailer: PHP\r\n".
-          "MIME-Version: 1.0\r\n".
-          "Content-Type: text/html; charset=UTF-8\r\n";
-      mb_language("uni");
-      mb_internal_encoding("UTF-8");
-      $subject64 = mb_encode_mimeheader($subject);
-      return mail($to, $subject64, $messageHTML, $headers);
+    $from = get_config('admin_email');
 
-    } else { //SMTP
-      require_once(__ROOT__ . 'php/external/swiftmailer-5.x/lib/swift_required.php');
-      $errorMail = !Swift_Validate::email($to);
-      if(isset($options['reply_to'])){
-        $replyToMails = explode(",",$options['reply_to']);
-        for ($i = 0; $i < count($replyToMails) && $errorMail == false ; $i++) {
-          if(!Swift_Validate::email($replyToMails[$i])) $errorMail = true;
+    if (!get_config('email_SMTP_host')) { // Send using mail() of PHP
+        $headers = 
+            'From: '.$from."\r\n".
+            'Reply-To: '.
+                (isset($options['reply_to']) ? $options['reply_to'] : $from)."\r\n".
+            (isset($options['cc']) ? 'Cc:'.$options['cc']."\r\n" : '').
+            (isset($options['bcc']) ? 'Bcc:'.$options['bcc']."\r\n" : '').
+            'Return-Path: '.$from."\r\n".
+            "X-Mailer: PHP\r\n".
+            "MIME-Version: 1.0\r\n".
+            "Content-Type: text/html; charset=UTF-8\r\n";
+        mb_language("uni");
+        mb_internal_encoding("UTF-8");
+        $subject64 = mb_encode_mimeheader($subject);
+        return mail($to, $subject64, $messageHTML, $headers);
+    } elseif (get_config('email_SMTP_host') === 'debug') { // Don't send, only dump!
+        return !!file_put_contents(
+            __ROOT__ . '/local_config/debug_mail/mail_' . date("Y-m-d_H-i-s") . '.html',
+            "<!DOCTYPE html><html>
+            <head><meta charset='utf-8'></head>
+            <body>
+                To: {$to}<br>
+                From: {$from}<br>
+                Options:<pre style='margin: 0 0 0 3em'>" .
+                    var_export($options, true) . "</pre>
+            <h1 style=\"background-color:#bbb;padding:3px\">{$subject}</h1>
+            {$messageHTML}
+            </body></html>"
+        );
+    } else { // Send using swiftmailer
+        require_once(__ROOT__ . 'php/external/swiftmailer-5.x/lib/swift_required.php');
+        $transport = Swift_SmtpTransport::newInstance(
+            get_config('email_SMTP_host'), 
+            get_config('email_SMTP_port', 25)
+        );
+        $encryp = get_config('email_SMTP_encryption');
+        if ($encryp) {
+            if (!in_array($encryp, stream_get_transports())) {
+                throw new Exception("config['email_SMTP_encryption']=\"{$encryp}\" is not supported on your hosting");
+            }
+            $transport->setEncryption($encryp);
+            if (!get_config('email_SMTP_verifyCert')) {
+                // See at the end of: https://github.com/swiftmailer/swiftmailer/issues/544
+                $https['ssl']['verify_peer'] = false;
+                $https['ssl']['verify_peer_name'] = false; // seems to work fine without this line so far
+                $transport->setStreamOptions($https);
+            }
         }
-      }
-      if(isset($options['cc'])){
-        $ccMails = explode(",",$options['cc']);
-        for ($i = 0; $i < count($ccMails) && $errorMail == false ; $i++) {
-          if(!Swift_Validate::email($ccMails[$i])) $errorMail = true;
+        if (get_config('email_SMTP_pswd')) {
+            $transport
+                ->setUsername(get_config('email_SMTP_user', $from))
+                ->setPassword(get_config('email_SMTP_pswd'));
         }
-      }
-      if(!$errorMail){
-        $transport = Swift_SmtpTransport::newInstance($cfg->email_SMTP_host, $cfg->email_SMTP_port)
-            ->setUsername($cfg->email_SMTP_user)
-            ->setPassword($cfg->email_SMTP_pswd)
-            ;
         $mailer = Swift_Mailer::newInstance($transport);
         $message = Swift_Message::newInstance()
-            ->setFrom(array($cfg->email_SMTP_user => $cfg->email_SMTP_name))
-            ->setTo($to)
             ->setSubject($subject)
-            ->setBody($messageHTML, 'text/html')
-            ;
-        if(isset($options['reply_to'])){
-          $message->setReplyTo($replyToMails);
+            ->setBody($messageHTML, 'text/html');
+        try {
+            $message->setFrom($from);
+        } catch(Exception $e) {
+            throw new Exception("config['admin_email']=\"{$from}\" is not a valid email");
+        } 
+        try {
+            $message->setTo(explode(',', $to));
+        } catch(Exception $e) {
+            throw new Exception("send_mail to: \"{$to}\" is not a list of valid emails");
+        } 
+        if (isset($options['reply_to']) && $options['reply_to']){
+            try {
+                $message->setReplyTo(explode(',', $options['reply_to']));
+            } catch(Exception $e) {
+                throw new Exception("send_mail reply_to: {$options['reply_to']} is not a list of valid emails.");
+            }
         }
-        if(isset($options['cc'])){
-          $message->setCc($ccMails);
+        if (isset($options['cc']) && $options['cc']) {
+            try {
+                $message->setCc(explode(',', $options['cc']));
+            } catch(Exception $e) {
+                throw new Exception("send_mail cc: {$options['cc']} is not a list of valid emails.");
+            }
         }
-        return $mailer->send($message);
-      }else{
-        return false;
-      }
+        if (isset($options['bcc']) && $options['bcc']) {
+            try {
+                $message->setBcc(explode(',', $options['bcc']));
+            } catch(Exception $e) {
+                throw new Exception("send_mail bcc: {$options['bcc']} is not a list of valid emails.");
+            }
+        }
+        return !!$mailer->send($message);
     }
-    return false;
 }
 
 /**
