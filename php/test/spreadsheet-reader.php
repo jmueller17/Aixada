316b9c258fa5ebfdb7bd6ca26117e10fa95cfd7b
jorix <xavier.mamano@gmail.com>
2022-02-28 19:26:16 +0100

    Set links to *spreadsheet-reader* test.

---
+++ php/test/spreadsheet-reader.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Tests of Spreadsheet-Reader</title>
+    <style>
+        .container {margin-right:auto;margin-left:auto;width:500px; font-size: 120%;}
+    </style> 
+</head>
+<body>
+    <div class="container">
+        <h1>Tests of Spreadsheet-Reader</h1>
+        <ul>
+            <li><a href="../../external/php53_2/spreadsheet-reader/test_import/test_import.php">Test code for php-5.3.2 to php-7.3</a></li>
+            <li><a href="../../external/php74/spreadsheet-reader/test_import/test_import.php">Test code for php-7.4 at least</a></li>
+        </ul>
+    </div>
+</body>
+</html>
